dfa1678a9c3c5cad18739ae9f597246cab762bb9
gamer78 <harris.mre@gmail.com>
2014-12-10 19:59:45 +0530

    Upload

---
+++ Project 78/Assets/Scripts/playerControl.cs
@@ -0,0 +1,25 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour 
+{
+	[SerializeField] Transform leftPunchButton;
+	[SerializeField] Transform rightPunchButton;
+	[SerializeField] int speed;
+	
+	
+	void Update (){
+		for(int i = 0; i < Input.touchCount; i++){
+			if(leftPunchButton.guiTexture.HitTest(Input.GetTouch(i).position)){
+				Debug.Log("punch left");
+			}
+			else if(rightPunchButton.guiTexture.HitTest(Input.GetTouch(i).position)){
+				Debug.Log("punch right");
+			}
+			else{
+				transform.LookAt(Input.GetTouch(i).position);
+				rigidbody2D.velocity = new Vector2(speed,0);
+			}
+		}
+	}
+}
